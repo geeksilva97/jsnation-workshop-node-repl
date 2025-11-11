fea75e8d1c2f8146b32ff4170d14ae318ccfcd70
Edy Silva <edigleyssonsilva@gmail.com>
2025-11-11 15:12:53 -0300

    chore: setup challenge 1 - starter template and workshop documentation
    Remove console.ts implementation and add TODOs for challenge 1.
    Add comprehensive workshop documentation covering all challenges.

--- park42/src/console.ts
+++ park42/src/console.ts
@@ -2,37 +2,25 @@ import * as repl from "node:repl";
 import { config } from "./config.js";
 import { makeDatabase } from "./infrastructure/database/database.js";
 import models from "./infrastructure/database/models/index.js";
-import { bindModels } from "./interface/console/index.js";
 
 const database = makeDatabase();
-const isSandbox = process.argv.includes("--sandbox");
 
 const startREPL = async () => {
   const r = repl.start("jsnation> ");
-  const trx = isSandbox ? await database.connection.transaction() : null;
-  let m = models;
-  if (trx) {
-    m = bindModels(trx);
-  }
 
-  Object.assign(r.context, {
-    name: "JSNation",
-    config,
-    ...m,
-  });
+  // TODO: Add the context variables here using Object.assign()
+  // Include:
+  // - name: "JSNation"
+  // - config
+  // - all models
+  // Example: Object.assign(r.context, { name: "JSNation", config, ...models });
 
-  r.defineCommand("mycommand", {
-    help: "Prints Hello Node REPL",
-    action() {
-      console.log("Hello Node REPL");
-      this.displayPrompt();
-    },
-  });
+  // TODO: Define the .mycommand custom command
+  // Use r.defineCommand() to define a command that:
+  // - Prints "Hello Node REPL" using console.log()
+  // - Calls this.displayPrompt() to show the prompt again
 
   r.on("exit", async () => {
-    if (isSandbox && trx) {
-      await trx.rollback();
-    }
     await database.disconnect();
     process.kill(process.pid, "SIGINT");
   });
